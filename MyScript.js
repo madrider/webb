c537de8de559179346f4516c97bfdad8a918b1a1
madrider <35990671+madrider@users.noreply.github.com>
2018-02-10 12:41:38 +0530

    Add files via upload

---
+++ MyScript.js
@@ -0,0 +1,11 @@
+//External Javascript File
+
+//General Message 
+document.write("This Message is from External Javascript File");
+
+function showDate()
+{
+		var date = new Date();
+		
+		document.write("Date is "+date);
+}
